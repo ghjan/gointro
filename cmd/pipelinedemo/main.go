95f0d2c0500a6617532d70ad981c6d8ed30c010a
jian zhang <jian.zhang@fuwo.com>
2018-04-13 17:10:21 +0800

    3-2 基础节点

---
+++ cmd/pipelinedemo/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/ghjan/gointro/pipeline"
+	"fmt"
+)
+
+func main() {
+	//demo1()
+	demo2()
+}
+func demo1() {
+	p := pipeline.ArraySource(3, 2, 6, 7, 4)
+	//for {
+	//	if num, ok := <-p; ok {
+	//		fmt.Println(num)
+	//	}else{
+	//		break
+	//	}
+	//}
+	//下面是比较简便的写法 使用range，但是发送方里面的p里面必须close，否则就不知道什么时候结束了
+	for v := range p {
+		fmt.Println(v)
+	}
+}
+
+func demo2() {
+	p := pipeline.InMemSort(pipeline.ArraySource(3, 2, 6, 7, 4))
+	for v := range p {
+		fmt.Println(v)
+	}
+}
---
+++ pipeline/nodes.go
@@ -0,0 +1,34 @@
+package pipeline
+
+import "sort"
+
+func ArraySource(a ...int) <-chan int { //用户只能拿东西
+	out := make(chan int)
+	go func() {
+		for _, v := range a {
+			out <- v
+		}
+		close(out)
+	}()
+	return out
+}
+
+func InMemSort(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		a := []int{}
+		for v := range in {
+			a = append(a, v)
+		}
+
+		//sort
+		sort.Ints(a)
+
+		// Output
+		for _, v := range (a) {
+			out <- v
+		}
+		close(out)
+	}()
+	return out
+}
