2e2937273b7fc09b050723d17afe11f756ed69af
jian zhang <jian.zhang@fuwo.com>
2018-04-13 17:17:54 +0800

    3-3 归并节点

--- cmd/pipelinedemo/main.go
+++ cmd/pipelinedemo/main.go
@@ -25,7 +25,8 @@ func demo1() {
 }
 
 func demo2() {
-	p := pipeline.InMemSort(pipeline.ArraySource(3, 2, 6, 7, 4))
+	p := pipeline.Merge(pipeline.InMemSort(pipeline.ArraySource(3, 2, 6, 7, 4)),
+		pipeline.InMemSort(pipeline.ArraySource(7, 4, 0, 3, 2, 13, 8)))
 	for v := range p {
 		fmt.Println(v)
 	}
